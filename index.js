f19d5b4464b1e0e9a75c4f8faa6a2c327684765e
Rani <rani76@gmail.com>
2017-11-06 15:40:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function happyHolidayTo (holiday, name) {
 }
 
 function holidayCountdown (days, holiday) {
-  return `It\'s  ${holiday} days until ${days}!`
+  return `It\'s ${holiday} days until ${days}!`
 }
