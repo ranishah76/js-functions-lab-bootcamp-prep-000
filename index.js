04526fcdb30c46ec33be9575bdcb109dbc3e0031
Rani <rani76@gmail.com>
2017-11-06 14:43:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function happyHolidays () {
-    return `Happy Holiday!`
+    return `Happy holidays!`
 }
 
 function happyHolidaysTo (name) {
-  return `Happy Holidays, ${name}`
+  return `Happy holidays, ${name}`
 }
 
 function happyHolidaysTo (name, holiday) {
