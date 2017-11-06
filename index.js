eba78f2a40ded979ff0c0a33d34a218c008227ff
Rani <rani76@gmail.com>
2017-11-06 13:22:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// write your code below!
+function happyHolidays () {
+    return 'Happy Holiday!'            
+}
+
+function happyHolidaysTo ('name') {
+  return 'Happy Holidays, ${name}'
+}
+
+function happyHolidaysTo ('name', 'holiday') {
+  return 'Happy $(holiday), $(name)'
+}
+
+function holidayCountdown ('name', 'holiday') {
+  return 'It\'s   $(days) days until $(holiday)!'
+}
+
